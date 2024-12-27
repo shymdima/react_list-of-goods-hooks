92a478f59f155539618f5cb4d0ecb44df8b25bde
dima <shymdima74@gmail.com>
2024-12-27 09:33:52 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import classNames from 'classnames';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -15,35 +16,116 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+
+
 export const App: React.FC = () => {
+  const [sortedGoods, setSortedGoods] = useState([...goodsFromServer]);
+  const [reversePressed, setReversePressed] = useState(false);
+  const [alphabeticallyPressed, setAlphabetically] = useState(false);
+  const [byLengthPressed, setByLength] = useState(false);
+
+  const sortGoods = (sortType: string) => {
+    let sorted = [...sortedGoods];
+
+    switch (sortType) {
+      case 'alphabetically':
+        sorted.sort((a, b) => {
+          setAlphabetically(!alphabeticallyPressed);
+          if (byLengthPressed) {
+            setByLength(!byLengthPressed);
+          }
+          if (reversePressed) {
+            return a.localeCompare(b) * -1;
+          } else {
+            return a.localeCompare(b)
+          }
+        })
+        break;
+
+      case 'byLength':
+        sorted.sort((a, b) => {
+          setByLength(!byLengthPressed);
+          if (alphabeticallyPressed) {
+            setAlphabetically(!alphabeticallyPressed);
+          }
+          if (reversePressed) {
+            return (a.length - b.length) * -1;
+          } else {
+            return a.length - b.length
+          }
+        })
+        break;
+
+      case 'reverse':
+        sorted.reverse();
+        setReversePressed(!reversePressed);
+        break;
+
+      default:
+        setAlphabetically(false)
+        setByLength(false)
+        setReversePressed(false)
+        sorted = [...goodsFromServer];
+    }
+
+    setSortedGoods(sorted);
+  };
+
+  const renderResetButton = () => {
+    return (
+      <button type="button" 
+      className="button is-danger is-light"
+      onClick={() => {
+        sortGoods('');
+      }}
+      >
+        Reset
+      </button>
+    )
+   }
+
   return (
     <div className="section content">
       <div className="buttons">
-        <button type="button" className="button is-info is-light">
+        <button type="button" 
+        className={classNames("button is-info", {"is-light": alphabeticallyPressed === false})}
+        onClick={() => {
+          sortGoods('alphabetically');
+        }}
+        >
           Sort alphabetically
         </button>
 
-        <button type="button" className="button is-success is-light">
+        <button type="button" 
+        className={classNames("button is-success", {"is-light": byLengthPressed === false})}
+        onClick={() => {
+          sortGoods('byLength');
+        }}
+        >
           Sort by length
         </button>
 
-        <button type="button" className="button is-warning is-light">
+        <button type="button" 
+        className={classNames("button is-warning", {"is-light": reversePressed === false})}
+        onClick={() => {
+          sortGoods('reverse');
+        }}
+        >
           Reverse
         </button>
 
-        <button type="button" className="button is-danger is-light">
-          Reset
-        </button>
+        {
+        JSON.stringify(sortedGoods) !== JSON.stringify(goodsFromServer)
+          ? renderResetButton()
+          : null}
+
+       
       </div>
 
       <ul>
         <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
+          {sortedGoods.map(good =>  (<li data-cy="Good" key = {good}>{good}</li>))}
+          
         </ul>
       </ul>
     </div>
